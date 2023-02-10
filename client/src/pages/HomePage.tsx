b56a4d5632434ea30544511b492e7398bfa386de
0xRin <73491185+0xRin@users.noreply.github.com>
2023-02-10 10:15:42 -0500

    refactor: add routing, create home page component and header component

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+
+type Props = {};
+
+const HomePage = (props: Props) => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default HomePage;
