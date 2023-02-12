b008e1541a2ee3252accf4d452934fcf194db69b
0xRin <73491185+0xRin@users.noreply.github.com>
2023-02-12 10:44:44 -0500

    fix: rename photo property of created place object to match model property

--- server/controllers/placeController.ts
+++ server/controllers/placeController.ts
@@ -5,12 +5,14 @@ import { AuthRequest } from "../middleware/authenticateUserMiddleware";
 export const createPlace = async (req: AuthRequest, res: Response) => {
     // grab form data
     const data = req.body;
+    // rename to match model property name
+    const { addedPhotos: photos } = data;
 
     // grab authenticated user info
     const user = req.userInfo
 
     // create new place
-    const newPlace = await Place.create({ owner: user.id, ...data });
+    const newPlace = await Place.create({ owner: user.id, photos, ...data });
 
     res.status(200).json(`${newPlace.title} created!`);
 }
