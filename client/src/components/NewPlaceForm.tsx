89794387c745dcb426dcaa90d309afe53f5a72bb
0xRin <73491185+0xRin@users.noreply.github.com>
2023-02-12 21:00:02 -0500

    feat: delete photos and select main photo in edit mode

--- client/src/components/NewPlaceForm.tsx
+++ client/src/components/NewPlaceForm.tsx
@@ -148,6 +148,23 @@ const NewPlaceForm = ({ edit }: Props) => {
     }
   };
 
+  const deletePicture = (selectedPhoto: string) => {
+    setPlaceForm((prev) => ({
+      ...prev,
+      addedPhotos: prev.addedPhotos.filter((photo) => photo !== selectedPhoto),
+    }));
+  };
+
+  const setAsMainPhoto = (selectedPhoto: string) => {
+    setPlaceForm((prev) => ({
+      ...prev,
+      addedPhotos: [
+        selectedPhoto,
+        ...placeForm.addedPhotos.filter((photo) => photo !== selectedPhoto),
+      ],
+    }));
+  };
+
   return (
     <div>
       <h1 className="mb-4 text-xl font-bold text-center">
@@ -211,12 +228,57 @@ const NewPlaceForm = ({ edit }: Props) => {
         <div className="grid grid-col-3 lg:grid-cols-6 sm:grid-cols-4 mt-2 text-2xl text-gray-600 mb-4 gap-2">
           {placeForm.addedPhotos.length > 0 &&
             placeForm.addedPhotos.map((photo) => (
-              <div className="lg:h-40 md:h-50 sm:h-60  flex">
+              <div
+                key={photo}
+                className="lg:h-40 md:h-50 sm:h-60  flex relative"
+              >
                 <img
                   className="rounded-2xl w-full object-cover"
                   src={`http://localhost:3000/uploads/${photo}`}
                   alt=""
                 />
+                <div
+                  onClick={() => setAsMainPhoto(photo)}
+                  className="absolute text-white cursor-pointer bg-black bg-opacity-40 rounded-full p-[0.4rem] left-1 top-1"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className={
+                      placeForm.addedPhotos[0] === photo
+                        ? `text-yellow-300 w-5 h-5`
+                        : `w-5 h-5`
+                    }
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
+                    />
+                  </svg>
+                </div>
+                <div
+                  onClick={() => deletePicture(photo)}
+                  className="absolute text-white cursor-pointer bg-black bg-opacity-40 rounded-full p-[0.4rem] right-1 top-1"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                    />
+                  </svg>
+                </div>
               </div>
             ))}
           <label className="lg:h-40 md:h-50 sm:h-60 flex gap-1 justify-center items-center border bg-transparent rounded-2xl p-2 cursor-pointer">
